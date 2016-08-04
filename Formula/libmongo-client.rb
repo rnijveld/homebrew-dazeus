9e8123722e738c14b797d7b1d64a985a36e0ee98
Ruben Nijveld <ruben@gewooniets.nl>
2016-08-04 15:50:07 +0200

    Update to sha256 for newer homebrew versions

--- Formula/libmongo-client.rb
+++ Formula/libmongo-client.rb
@@ -3,7 +3,7 @@ require "formula"
 class LibmongoClient < Formula
   homepage "https://github.com/algernon/libmongo-client"
   url "https://github.com/algernon/libmongo-client/archive/libmongo-client-0.1.7.tar.gz"
-  sha1 "5981aeaa9466a7db53be9a8ef9868ece1ff4a893"
+  sha256 "820d98d1ee06070aa51861d1583dd5b3556b6dbca6fecdc1c91eb3c18360d2d9"
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
